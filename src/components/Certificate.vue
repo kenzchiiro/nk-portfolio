2a09c3c01080e529da916251b835171f7ec36288
kench1r0 <nattawut.eleven@gmail.com>
2022-06-01 23:26:33 +0700

    [Edited] add certificate

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -1,24 +1,32 @@
 <template>
     <v-row dense>
-    <v-col xs="12" sm="6" md="4" lg="3" cols="12" :class="'layer-' + layerClass" v-for="(event, e) in data" :key="e">
-      <v-card data-aos="fade-up" :data-aos-duration="(e+2)*200"  class="inline" min-width="150" outlined>
-        <v-row dense>
-            <v-col xs="1" sm="5" md="5" lg="5" xl="5" cols="4">
+    <v-col xs="12" sm="12" md="12" lg="4" cols="12" :class="'layer-' + layerClass" v-for="(event, e) in data" :key="e">
+      <v-card data-aos="fade-up" :data-aos-duration="(e+2)*200"  class="inline" min-width="100" max-height="120" outlined>
+        <v-row dense >
+          <!-- <div> -->
+            <v-col class="certificate-icon" cols="12">
+             <v-avatar
+                class="ma-3"
+                size="118"
+                tile
+              >
             <v-img
                 style="border-top-left-radius: 3%; border-bottom-left-radius: 3%;"
-                height="100%"
-                width="100"
                 :src="pathLogo(event.icon)"
             ></v-img>
-            </v-col>
-            <v-col xs="11" sm="7" md="7" lg="7" xl="7" >
-            <v-list-item-content>
-            <div class="title">
-                <p><bold v-for="(line,l) in event.topic" :key="l">{{line}}</bold>   <v-icon style="float:right; margin-right: 0.5rem; color:var(--v-info-base);">mdi-file-certificate-outline</v-icon></p>
-            </div>
-            <v-list-item-title class="level">{{event.level}}</v-list-item-title>
-            <a class="code" v-bind:href="event.link">{{event.id}} </a>
-            </v-list-item-content>
+            </v-avatar>
+              <!-- <v-icon style="float:right; margin-right: 0.5rem; color:var(--v-info-base);">mdi-file-certificate-outline</v-icon> -->
+                    <v-row dense style="padding: 10px;">
+                        <v-col cols="12">
+                          <v-card-subtitle class="certificate-title" style="padding: 0px !important; padding-top: 0.5rem;" v-text="event.topic"></v-card-subtitle>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-card-subtitle class="certificate-subtitle" style="padding: 0px !important;" v-text="event.level"></v-card-subtitle>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-card-subtitle class="certificate-title" style="padding: 0px !important;"><a class="code" v-bind:href="event.link">{{event.id}} </a> </v-card-subtitle>
+                        </v-col>
+                    </v-row>
             </v-col>
         </v-row>
       </v-card>
