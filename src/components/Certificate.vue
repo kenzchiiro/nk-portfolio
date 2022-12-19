df992f4e1b10f23d9eb97efc9c5471a4fd8239dd
kench1r0 <nattawut.eleven@gmail.com>
2022-12-19 22:30:18 +0700

    [Edited] update

--- src/components/Certificate.vue
+++ src/components/Certificate.vue
@@ -21,7 +21,10 @@
                           <v-card-subtitle class="certificate-title" style="padding: 0px !important; padding-top: 0.5rem;" v-text="event.topic"></v-card-subtitle>
                         </v-col>
                         <v-col cols="12">
-                          <v-card-subtitle class="certificate-subtitle" style="padding: 0px !important;" v-text="event.level"></v-card-subtitle>
+                          <v-card-subtitle class="certificate-subtitle" style="padding: 0px !important;">
+                            <span v-text="event.level"></span>
+                            <v-icon small v-b-tooltip.hover :title=event.tooltips style="margin-left:0.2rem; !important; padding-bottom: 0.1rem;">mdi-information-outline</v-icon>
+                          </v-card-subtitle>
                         </v-col>
                         <v-col cols="12">
                           <v-card-subtitle class="certificate-title" style="padding: 0px !important;"><a class="code" v-bind:href="event.link">{{event.id}} </a> </v-card-subtitle>
@@ -38,7 +41,7 @@
 export default {
   name: "Card",
   props: ["title", "data"],
-    methods: {
+  methods: {
     pathLogo: function (path) {
       console.log(require("@/" + path));
       return require("@/" + path);
